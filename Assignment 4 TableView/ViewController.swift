7793a97ac5fb9def8f2116127d8c4ff22e054272
KaranPatel19 <41769712+KaranPatel19@users.noreply.github.com>
2022-12-05 16:27:15 -0500

    initial commit

---
+++ Assignment 4 TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 4 TableView
+//
+//  Created by Karan Patel on 2022-12-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
