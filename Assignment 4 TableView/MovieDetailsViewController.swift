0ed0689bac18cac2d733f810f87bc6a662cf1c43
KaranPatel19 <41769712+KaranPatel19@users.noreply.github.com>
2022-12-05 19:15:57 -0500

    Added Navigation controller and view contoller

---
+++ Assignment 4 TableView/MovieDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsViewController.swift
+//  Assignment 4 TableView
+//
+//  Created by Karan Patel on 2022-12-05.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
